e94732e964b27023848e8f70fd0e2974b136b1eb
Mah-eshh <92861@fhss.sjp.ac.lk>
2021-12-25 12:37:35 +0530

    First commit

---
+++ src/main/java/com/mahezh/student/StudentRegistration.java
@@ -0,0 +1,19 @@
+package com.mahezh.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class StudentRegistration {
+
+	
+	public static void main(String[] args) {
+		
+		 
+		 SpringApplication.run(StudentRegistration.class, args);
+		 System.out.println("It is running port on :8080");
+
+	}
+
+}
