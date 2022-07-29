74a6a282d20e5b4b081e19cbac8b5e3338eeb016
maheshabeykoon <maheshpabeykoon@gmail.com>
2022-07-29 19:30:24 +0530

    Add Validation for Students details

--- src/main/java/com/mahezh/student/model/Student.java
+++ src/main/java/com/mahezh/student/model/Student.java
@@ -6,6 +6,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "student")
@@ -17,18 +22,38 @@ public class Student {
 	
 	private long id;
 	
-	@Column(name ="student_name", nullable = false)
+
+	@NotBlank(message = "Invalid Name: Empty name")
+	@NotNull(message = "Invalid Name: Name is NULL")
+	@Size(min = 3, max = 30, message = "Invalid Name: Must be of 3 - 15 characters")
+	
+	@Column(name ="student_name")
 	private String studentName;
 	
-	@Column(name = "age",  nullable = false)
+	
+	@NotBlank(message = "Invalid Age: Empty age")
+	@NotNull(message = "Invalid Age: Age is NULL")
+	@Min(value = 6, message = "Invalid Age: Equals to five or Less than five")
+    @Max(value = 20, message = "Invalid Age: Exceeds 20 years")
+
+	@Column(name = "age")
 	private int   age;
 	
+
+	@NotBlank(message = "Invalid Address: Empty address")
+
 	@Column(name = "address",  nullable = false)
 	private String  address;
+	
+
+
+	@NotBlank(message = "Invalid Birthday: Empty Birthday")
+	@NotNull(message = "Invalid Birthday: Birthday is NULL")
 	
 	@Column(name = "birthDay",  nullable = false)
 	private String birthDay;
 	
+
 	public long getId () {
 		return id;
 	}
